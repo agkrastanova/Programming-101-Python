4de932ac397a73fb5e5df18bbf163e0649c52d9e
Ann <krastanova98@gmail.com>
2020-05-01 17:25:04 +0300

    fixed

--- week09/task_pc.sql
+++ week09/task_pc.sql
@@ -2,7 +2,7 @@ SELECT AVG(speed) FROM pc;
 
 SELECT AVG(screen) FROM laptop JOIN product ON laptop.model = product.model GROUP BY maker;
 
-SELECT AVG(speed) FROM (SELECT * FROM laptop WHERE price > 1000); 
+SELECT AVG(speed) FROM laptop WHERE price > 1000; 
 
 SELECT AVG(price) FROM pc GROUP BY hd;
 
@@ -10,7 +10,7 @@ SELECT AVG(price) FROM (SELECT * FROM pc WHERE speed > 500);
 
 SELECT AVG(price) FROM pc JOIN product ON pc.model = product.model WHERE product.maker = 'A'; 
 
-SELECT AVG(price) FROM pc JOIN product ON pc.model = product.model WHERE product.maker = 'B' UNION SELECT AVG(price) FROM laptop JOIN product ON laptop.model = product.maker WHERE product.maker = 'B'; 
+SELECT AVG(price) FROM pc JOIN product ON pc.model = product.model WHERE product.maker = 'B' UNION SELECT AVG(price) FROM laptop JOIN product ON laptop.model = product.model WHERE product.maker = 'B'; 
 
 SELECT * FROM (SELECT COUNT(code) FROM pc JOIN product ON pc.model = product.model GROUP BY product.maker); 
 
